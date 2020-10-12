df79114a1fa8fa83849dc2caad45834f3632cddd
Los Payitos <los_payitos@Loss-MacBook-Pro.local>
2020-10-11 21:18:06 -0500

    Add last data creation for mongo

--- src/main/java/com/basesMongoJava/app/InitializeMongo.java
+++ src/main/java/com/basesMongoJava/app/InitializeMongo.java
@@ -12,6 +12,14 @@ public class InitializeMongo {
      */
     public static void initializeCollections(){
         MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        mongoClient.getDatabase("local").getCollection("sucursal").drop();
+        mongoClient.getDatabase("local").getCollection("ciudad").drop();
+        mongoClient.getDatabase("local").getCollection("departamento").drop();
+        mongoClient.getDatabase("local").getCollection("pais").drop();
+        mongoClient.getDatabase("local").getCollection("vendedor").drop();
+        mongoClient.getDatabase("local").getCollection("gremio").drop();
+        mongoClient.getDatabase("local").getCollection("producto").drop();
+        mongoClient.getDatabase("local").getCollection("marca").drop();
         mongoClient.getDatabase("local").createCollection("sucursal");
         mongoClient.getDatabase("local").createCollection("ciudad");
         mongoClient.getDatabase("local").createCollection("departamento");
@@ -167,25 +175,43 @@ public class InitializeMongo {
         MongoDatabase.upsert(collection, clavePrimaria, documento);
 
         collection = "marca";
+        clavePrimaria = "nombre";
 
         documento = new HashMap<String, Object>(){{
-            put("nombre", "Pinky");
-            put("codigo", "V3");
+            put("nombre", "Adidas");
+            put("descripcion", "Ropita en general :v");
+            put("ventas", 2000);
+        }};
+        MongoDatabase.upsert(collection, clavePrimaria, documento);
+
+        documento = new HashMap<String, Object>(){{
+            put("nombre", "Niki");
+            put("descripcion", "Copias de Nike :)");
             put("ventas", 9000);
         }};
         MongoDatabase.upsert(collection, clavePrimaria, documento);
 
         documento = new HashMap<String, Object>(){{
-            put("nombre", "Pinky");
-            put("codigo", "V3");
-            put("ventas", 9000);
+            put("nombre", "Marca vacia");
+            put("descripcion", "No tenemos producto");
+            put("ventas", 0);
+        }};
+        MongoDatabase.upsert(collection, clavePrimaria, documento);
+
+        collection = "producto";
+        clavePrimaria = "codbarras";
+
+        documento = new HashMap<String, Object>(){{
+            put("nombre", "Tenis deportivos");
+            put("codbarras", "PKi9");
+            put("ventas", 0);
         }};
         MongoDatabase.upsert(collection, clavePrimaria, documento);
 
         documento = new HashMap<String, Object>(){{
-            put("nombre", "Pinky");
-            put("codigo", "V3");
-            put("ventas", 9000);
+            put("nombre", "Producto 1");
+            put("codbarras", "CodBarras1");
+            put("ventas", 0);
         }};
         MongoDatabase.upsert(collection, clavePrimaria, documento);
     }
